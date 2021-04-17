644fc2ed98cde96d3bed05680b47de6eda2d6da7
shellboy <qkrdmstlr3@naver.com>
2021-04-18 01:03:47 +0900

    [fix] electron app크기 조정 및 css 조정
    - electron app의 크기를 알맞게 조정
    - css에서 부족한 부분 추가
    - 불필요한 파일 제거

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,8 @@ const isDev = process.env.NODE_ENV === 'development';
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1200,
-    height: 800,
+    height: 660,
+    titleBarStyle: 'hidden',
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
