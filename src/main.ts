146905e6bac190eb3cb9e0dc2c3358493df27528
shellboy <qkrdmstlr3@naver.com>
2021-04-27 00:26:49 +0900

    [feat] electron updater를 이용한 자동 업데이트 추가
    - electron updater를 이용해서 자동 업데이트를 추가하였다
    - 버전은 0.0.1로 수정하였다

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { app, BrowserWindow, ipcMain } from 'electron';
+import { autoUpdater } from 'electron-updater';
+import electronLog from 'electron-log';
 import path from 'path';
 
 let mainWindow: Electron.BrowserWindow | null;
@@ -33,5 +35,42 @@ function createWindow() {
   });
 }
 
-app.on('ready', createWindow);
+// updater
+autoUpdater.on('checking-for-update', () => {
+  electronLog.info('업데이트 확인 중...');
+});
+
+autoUpdater.on('update-available', () => {
+  electronLog.info('업데이트가 가능합니다');
+});
+
+autoUpdater.on('update-not-available', () => {
+  electronLog.info('현재 최신버전입니다');
+});
+
+autoUpdater.on('error', (err) => {
+  electronLog.info('에러가 발생했습니다 : ' + err);
+});
+
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = '다운로드 속도: ' + progressObj.bytesPerSecond;
+  log_message = log_message + ' - 현재 ' + progressObj.percent + '%';
+  log_message =
+    log_message +
+    ' (' +
+    progressObj.transferred +
+    '/' +
+    progressObj.total +
+    ')';
+  electronLog.info(log_message);
+});
+
+autoUpdater.on('update-downloaded', () => {
+  electronLog.info('업데이트가 완료되었습니다');
+});
+
+app.on('ready', () => {
+  createWindow();
+  autoUpdater.checkForUpdates();
+});
 app.allowRendererProcessReuse = true;
