bd4eb475c65299802c7ddada514b3402f2c0ee18
shellboy <qkrdmstlr3@naver.com>
2021-05-02 21:06:44 +0900

    [feat] SubnavCheck component에서 list조회 / 추가 기능 구현
    - SubnavCheck 기능구현
      - checklist state를 observing
      - 초기 로딩 시 checklist state에 아무것도 없다면 db에서 체크리스트목록 가져오는 요청 전달
      - 리스트 추가 버튼을 누르면 새로운 리스트 아이템이 생
      - render에서 item들은 일단 제거하였다

--- src/components/SubnavCheck/index.ts
+++ src/components/SubnavCheck/index.ts
@@ -1,13 +1,38 @@
-import { ShellHTML, createComponent, EventType } from '@Lib/shell-html';
+import {
+  ShellHTML,
+  createComponent,
+  EventType,
+  RenderType,
+  useGlobalState,
+  setGlobalState,
+} from '@Lib/shell-html';
 import styleSheet from './style.scss';
-import { dummyChecklist } from '../../common/data/dummyData';
+import { ipcRenderer } from 'electron';
+import short from 'short-uuid';
 
 class SubnavCheck extends ShellHTML {
   constructor() {
     super('');
   }
 
-  clickNameHandler(event: Event) {
+  connectedCallback(): void {
+    this.enrollObserving('checklist');
+
+    const list = useGlobalState('checklist');
+    if (!list.length) {
+      ipcRenderer.send('checklist:read:all');
+      ipcRenderer.once('checklist:read:all', (event, list) => {
+        setGlobalState('checklist', list);
+      });
+    }
+  }
+
+  disconnectedCallback(): void {
+    this.enrollObserving('checklist');
+    ipcRenderer.removeAllListeners('checklist:read:all');
+  }
+
+  clickAccordionHandler(event: Event): void {
     if (!(event.target instanceof HTMLElement)) return;
 
     const accordionTitleId = event.target.closest('.accordion__header')?.id;
@@ -18,41 +43,45 @@ class SubnavCheck extends ShellHTML {
     this.setState('');
   }
 
-  render() {
-    const listNames = Object.keys(dummyChecklist);
-    const list = listNames.reduce((acc: string, name: string) => {
-      return (acc += `
+  clickAddButtonHandler(event: Event): void {
+    if (!(event.target instanceof HTMLElement)) return;
+
+    const list = useGlobalState('checklist');
+    const newItem = { id: short.generate(), name: '새 목록' };
+
+    setGlobalState('checklist', [...list, newItem]);
+    ipcRenderer.send('checklist:add', newItem);
+  }
+
+  render(): RenderType {
+    const list = useGlobalState('checklist');
+    const listHTML = list.reduce(
+      (acc: string, { id, name }: { id: string; name: string }) => {
+        return (acc += `
       <li class="accordion">
         <header class="accordion__header ${
-          this.state === name ? 'choosed' : ''
-        }" id="${name}">
+          this.state === id ? 'choosed' : ''
+        }" id="${id}">
           <h3 class="accordion__name">${name}</h3>
           <span>▼</span>
         </header>
-        ${
-          this.state === name
-            ? `<ul class="accordion__list">
-          ${dummyChecklist[name].reduce((acc: string, { title, date }) => {
-            return (acc += `
-              <li class="accordion__item">
-                <h4 class="accordion__item__name ${title}">${title}</h4>
-                <span>${date}</span>
-              </li>
-            `);
-          }, '')}
-        </ul>`
-            : ''
-        }
       </li>
       `);
-    }, '');
+      },
+      ''
+    );
 
     return {
       css: styleSheet,
       eventFuncs: [
         {
           className: 'accordion__header',
-          func: this.clickNameHandler,
+          func: this.clickAccordionHandler,
+          type: EventType.click,
+        },
+        {
+          className: 'accordion__plus',
+          func: this.clickAddButtonHandler,
           type: EventType.click,
         },
       ],
@@ -63,7 +92,8 @@ class SubnavCheck extends ShellHTML {
           <button class="nav__top__button">+</button>
         </div>
         <ul class="nav__bottom">
-          ${list}
+          ${listHTML}
+          <li class="accordion__plus"> + </li>
         </ul>
       </nav>
       `,
