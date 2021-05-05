0c9312fc148b22a3ed174e3bbb02d0ee01bf4311
shellboy <qkrdmstlr3@naver.com>
2021-05-06 02:19:17 +0900

    [feat] CheckPost component에서 포스트 조회기능 구현
    - 렌더링 시 checkpostControl상태에서 현재 선택된 포스트의 id를 가져온다
    - id가 없다면 none을 렌더링
    - id가 있다면 checkposts상태에서 캐싱된 정보가 있는지 확인한다.
    - 없다면 ipc를 통해서 db에서 포스트 정보를 가져와서 렌더링한다.
    - 현재 렌더링하는 post는 컴포넌트 자체의 state를 이용한다.

--- src/components/CheckPost/index.ts
+++ src/components/CheckPost/index.ts
@@ -1,60 +1,86 @@
 import { ShellHTML, createComponent } from '@Lib/shell-html';
 import styleSheet from './style.scss';
-import { dummyCheckPost } from '../../common/data/dummyData';
+import { useGlobalState, setGlobalState } from '../../lib/shell-html/state';
+import { CheckPostType } from '../../types/types';
+import { CheckPostStatusType } from '../../types/enum';
+import { getDday } from '../../utils/calcDate';
+import { ipcRenderer } from 'electron';
 
 class CheckPost extends ShellHTML {
-  getPostStatus(status: string): string {
+  constructor() {
+    super(undefined);
+  }
+
+  connectedCallback() {
+    this.enrollObserving('checkpostControl');
+  }
+
+  disconnectedCallback() {
+    this.releaseObserving('checkpostControl');
+    ipcRenderer?.removeAllListeners('checkpost:getPost');
+  }
+
+  getPostStatus(status: CheckPostStatusType): string {
     switch (status) {
-      case 'todo':
+      case CheckPostStatusType.todo:
         return `<div class="post__status status__todo">x</div>`;
-      case 'doing':
+      case CheckPostStatusType.doing:
         return `<div class="post__status status__doing">⎯</div>`;
-      case 'done':
+      case CheckPostStatusType.done:
         return `<div class="post__status done">v</div>`;
       default:
         return '';
     }
   }
 
-  getDday(endDay: string): string {
-    const today = new Date();
-    const endDate = new Date(endDay);
-    const dday = Math.ceil(
-      (endDate.getTime() - today.getTime()) / (1000 * 3600 * 24)
-    );
-    if (dday == 0) return 'dday';
-    else if (dday < 0) return `+${dday}`;
-    else return `-${dday}`;
+  getPost(): void {
+    const postId = useGlobalState('checkpostControl').currentCheckPostId;
+    if (!postId || postId === this.state?.id) return;
+
+    const posts = useGlobalState('checkposts');
+    const [post] = posts.filter((post: CheckPostType) => post.id === postId);
+    if (post) {
+      this.setState(post);
+      return;
+    }
+
+    ipcRenderer?.send('checkpost:getPost', { id: postId });
+    ipcRenderer?.once('checkpost:getPost', (event, post) => {
+      setGlobalState('checkposts', [...posts, post]);
+      this.setState(post);
+    });
   }
 
   render() {
+    this.getPost();
+    const post = this.state;
+
     return {
       css: styleSheet,
-      html: `
+      html: post
+        ? `
       <div class="post">
         <header class="post__header">
           <div class="post__header__left">
             <div class="post__header__top">
-              ${this.getPostStatus(dummyCheckPost.status)}
-              <h1 class="post__header__title">제목</h1>
-              <span class="post__header__dday">${this.getDday(
-                dummyCheckPost.endDate
-              )}</span>
+              ${this.getPostStatus(post.status)}
+              <h1 class="post__header__title" contenteditable="true">제목</h1>
+              <span class="post__header__dday">${getDday(post.endDate)}</span>
             </div>
             <div class="post__header__bottom">
-              <input type="date" value=${dummyCheckPost.startDate} />
-              <input type="date" value=${dummyCheckPost.endDate} />
+              <input type="date" value="${post.startDate}" />
+              <input type="date" value="${post.endDate}" />
             </div>
           </div>
           <div class="post__header__right">
             <button>삭제</button>
-            <button>저장</button>
           </div>
         </header>
         <div class="post__content">
-          ${dummyCheckPost.content}
+          ${post.content}
         </div>
-      </div>`,
+      </div>`
+        : '<div>none</div>',
     };
   }
 }
