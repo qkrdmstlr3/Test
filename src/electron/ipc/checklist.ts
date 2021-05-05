d255ee6e25cfd45cf263c9608d08a3f58411808f
shellboy <qkrdmstlr3@naver.com>
2021-05-06 02:09:33 +0900

    [refactor] nedb collection이름 업데이트 및 checklist조회함수 수정
    - nedb collection이름을 좀 더 직관적이고 알맞게 변경
    - checklist들을 조회하는 함수 수정
      - list의 item들에 속하는 post들을 합쳐서 ipc통신으로 보내준다.
      - 타입때문에 로직이 더러워진 부분이 있어 리팩토링을 하면 좋을 것 같다

--- src/electron/ipc/checklist.ts
+++ src/electron/ipc/checklist.ts
@@ -1,12 +1,44 @@
 import { ipcMain, BrowserWindow } from 'electron';
 import { db } from '../db';
+import { getDday } from '../../utils/calcDate';
+import { PostType } from '../../types/types';
 
 export default function checkListIpcMain(mainWindow: BrowserWindow): void {
   ipcMain.on('checklist:read:all', async () => {
-    const lists = await db.checklist?.find({});
+    const lists =
+      (await db.checklist
+        ?.find({}, { id: 1, name: 1, updatedAt: 1 })
+        .sort({ updatedAt: 1 })) || [];
+    const posts =
+      (await db.checkpost
+        ?.find({}, { id: 1, title: 1, endDate: 1, listId: 1 })
+        .sort({ updatedAt: 1 })) || [];
+
+    const newList = [...Array(lists?.length || 0)];
+    const newListPlusPosts = newList.map((_, i) => {
+      const { id, name } = lists[i];
+      const newItem = {
+        id: id as string,
+        name: name as string,
+        posts: [] as Array<PostType>,
+      };
+      posts.map((post) => {
+        const { id: postId, title, endDate, listId } = post;
+        if (listId === id) {
+          const newPost = {
+            id: postId as string,
+            title: title as string,
+            dday: getDday(endDate as string),
+          };
+          newItem.posts.push(newPost);
+        }
+      });
+
+      return newItem;
+    });
 
     if (mainWindow) {
-      mainWindow.webContents.send('checklist:read:all', lists);
+      mainWindow.webContents.send('checklist:read:all', newListPlusPosts);
     }
   });
 
@@ -23,5 +55,6 @@ export default function checkListIpcMain(mainWindow: BrowserWindow): void {
 
   ipcMain.on('checklist:delete', async (event, data) => {
     await db.checklist?.remove({ id: data.id }, {});
+    await db.checkpost?.remove({ listId: data.id }, { multi: true });
   });
 }
