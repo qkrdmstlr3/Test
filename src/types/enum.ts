a60031c6e7e1a7d4ce544c8b1944a6828adc1edf
shellboy <qkrdmstlr3@naver.com>
2021-05-06 02:04:09 +0900

    [feat] 날짜계산 util파일 추가 및 type추가
    - 날짜계산을 저장할 calcDate파일 추가
      - dday를 계산하는 함수 구현
    - type추가
      - 요약된 checkpost정보와 checkpost전체 정보를 나타내는 타입 추가
      - 현재 진행 상태를 표시를 위해서 enum을 만들었다

---
+++ src/types/enum.ts
@@ -0,0 +1,5 @@
+export enum CheckPostStatusType {
+  todo = 'todo',
+  doing = 'doing',
+  done = 'done',
+}
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,9 +1,22 @@
+import { CheckPostStatusType } from './enum';
+
 export interface PostType {
+  id: string;
   title: string;
+  dday: string;
 }
 
 export interface CheckListType {
   id: string;
   name: string;
   posts: PostType[];
+}
+
+export interface CheckPostType {
+  id: string;
+  title: string;
+  status: CheckPostStatusType;
+  startDate: string;
+  endDate: string;
+  content: string;
 }
