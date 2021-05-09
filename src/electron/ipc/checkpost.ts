f6508dcc273d3d7bbac49d24b58208fb194f1964
shellboy <qkrdmstlr3@naver.com>
2021-05-10 02:43:44 +0900

    [feat] electron checkpost update ipc처리
    - checkpost update ipc통신에 대해서 처리
      - 기존 디비의 데이터와 비교해서 다른 점이 있다면 디비의 데이터를 업데이트 시킨다

--- src/electron/ipc/checkpost.ts
+++ src/electron/ipc/checkpost.ts
@@ -1,5 +1,6 @@
 import { ipcMain, BrowserWindow } from 'electron';
 import { db } from '../db';
+import { CheckPostType } from '../../types/types';
 
 export default function checkListIpcMain(mainWindow: BrowserWindow): void {
   ipcMain.on('checkpost:add', async (event, newPost) => {
@@ -11,6 +12,33 @@ export default function checkListIpcMain(mainWindow: BrowserWindow): void {
 
     if (mainWindow) {
       mainWindow.webContents.send('checkpost:getPost', post);
+    }
+  });
+
+  ipcMain.on('checkpost:update', async (event, data) => {
+    const post = (await (db.checkpost?.findOne({
+      id: data.id,
+    }) as unknown)) as CheckPostType;
+
+    const isChangeExits =
+      post.content !== data.content ||
+      post.endDate !== data.endDate ||
+      post.startDate !== data.startDate ||
+      post.status !== data.status ||
+      post.title !== data.title;
+    if (data && isChangeExits) {
+      await db.checkpost?.update(
+        { id: data.id },
+        {
+          $set: {
+            content: data.content,
+            endDate: data.endDate,
+            startDate: data.startDate,
+            status: data.status,
+            title: data.title,
+          },
+        }
+      );
     }
   });
 }
