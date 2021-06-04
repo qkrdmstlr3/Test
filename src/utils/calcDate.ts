e240e5e77e58e66290d0d6d54b07edb8158324f4
shellboy <qkrdmstlr3@naver.com>
2021-06-05 00:04:22 +0900

    [feat] home page nav에 시계 구현
    - home page의 nav에 시계를 구현
      - SubnavHome component에 시계구현
      - utils의 calcDate함수에 getToday함수 구현

--- src/utils/calcDate.ts
+++ src/utils/calcDate.ts
@@ -1,3 +1,5 @@
+const dayList = ['일', '월', '화', '수', '목', '금', '토'];
+
 export function getDday(endDay: string): string {
   const today = new Date();
   const endDate = new Date(`${endDay}T00:00:00`);
@@ -9,4 +11,17 @@ export function getDday(endDay: string): string {
   if (dday == 0) return 'dday';
   else if (dday < 0) return `+${-dday}`;
   else return `-${dday}`;
+}
+
+export function getToday(): (string | number)[] {
+  const today = new Date();
+
+  const hour = `${today.getHours() < 10 ? '0' : ''}${today.getHours()}`;
+  const minute = `${today.getMinutes() < 10 ? '0' : ''}${today.getMinutes()}`;
+  const year = today.getFullYear();
+  const month = today.getMonth() + 1;
+  const day = today.getDate();
+  const dow = dayList[today.getDay()];
+
+  return [hour, minute, year, month, day, dow];
 }
